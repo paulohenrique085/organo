b8e44ed471784fc8b1d559974f8f31f8f556b405
paulohenrique085 <paulohnq55@outlook.com>
2024-01-03 23:38:01 -0300

    update

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,19 +2,48 @@ import "./form.css";
 import TextField from "../textField/TextField";
 import OnClickButton from "../buttons/onClick/OnClickButton";
 import Select from "../select/Select";
+import { useState } from "react";
 
 const Form = () => {
+  const [name, setName] = useState("");
+  const [office, setOffice] = useState("");
+  const [image, setImage] = useState("");
+  const [team, setTeam] = useState("");
+
   const teams = ["BackEnd", "Produto", "FrontEnd"];
+
+  const handleSubmit = (event) => {
+    //faz com que o react não siga o comportamento padrão
+    event.preventDefault();
+    console.log(name, office, image);
+  };
+
   return (
-    <form className="form-container">
+    <form onSubmit={handleSubmit} className="form-container">
       <h2 className="form-title">Cadastro de colaboradores</h2>
-      <TextField placeholderValue={"Digite seu nome"} labelValue={"Nome"} />
-      <TextField placeholderValue={"Digite seu cargo"} labelValue={"Cargo"} />
       <TextField
+        value={name}
+        changedValue={(value) => setName(value)}
+        requiredField={true}
+        placeholderValue={"Digite seu nome"}
+        labelValue={"Nome"}
+      />
+      <TextField
+        value={office}
+        changedValue={(value) => setOffice(value)}
+        //define se é um campo obrigatório direto no html
+        requiredField={true}
+        placeholderValue={"Digite seu cargo"}
+        labelValue={"Cargo"}
+      />
+      <TextField
+        value={image}
+        changedValue={(value) => setImage(value)}
+        requiredField={true}
         placeholderValue={"Insira o endereço da imagem"}
         labelValue={"Imagem"}
       />
-      <Select options={teams} labelValue={"Time"} />
+      <Select requiredField={true} options={teams} labelValue={"Time"} />
 
       <OnClickButton titleButton={"Cadastrar Colaborador"} />
     </form>
--- src/components/textField/TextField.jsx
+++ src/components/textField/TextField.jsx
@@ -1,12 +1,20 @@
 import "./textField.css";
 
 const TextField = (props) => {
+  const changeValue = (event) => {
+    props.changedValue(event.target.value);
+  };
+
   return (
     <div className="textField-container">
       <label className="textField-label" htmlFor="">
         {props.labelValue}
       </label>
       <input
+        onChange={changeValue}
+        value={props.value}
+        //valida a obrigatoriedade do campo
+        required={props.requiredField}
         className="textField-input"
         type="text"
         placeholder={props.placeholderValue}
